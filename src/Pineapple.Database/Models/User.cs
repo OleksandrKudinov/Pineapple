bb1fed1c60ccab50a0471aca97ab396844b9cecb
Oleksandr Kudinov <oleksandr.kudinov.1993@gmail.com>
2017-06-15 02:48:32 +0300

    add model User

---
+++ src/Pineapple.Database/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Pineapple.Database
+{
+    public sealed class User
+    {
+        public Int32 UserId { get; set; }
+        public String Name { get; set; }
+    }
+}
