9e6af6e62180e9a9e8c903c9acb0795ef10c241a
Oleksandr Kudinov <oleksandr.kudinov.1993@gmail.com>
2017-06-15 02:48:01 +0300

    add basecontroller

---
+++ src/Pineapple.Service/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pineapple.Service.Controllers
+{
+    [Route("api/[controller]")]
+    public abstract class BaseController : Controller
+    {
+        protected String _connectionString = "";
+    }
+}
