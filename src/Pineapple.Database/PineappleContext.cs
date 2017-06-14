45924737192462d2a1a52dbd5c9dabe1000acf7e
Oleksandr Kudinov <oleksandr.kudinov.1993@gmail.com>
2017-06-14 23:21:42 +0300

    add Pineapple db context

---
+++ src/Pineapple.Database/PineappleContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.Entity;
+using System.Reflection;
+using Pineapple.Database.Models;
+
+namespace Pineapple.Database
+{
+    public sealed class PineappleContext : DbContext
+    {
+        public PineappleContext(String nameOrConnectionString) : base(nameOrConnectionString)
+        {
+            System.Data.Entity.Database.SetInitializer(new DropCreateDatabaseIfModelChanges<PineappleContext>());
+            //System.Data.Entity.Database.SetInitializer(new DropCreateDatabaseAlways<PineappleContext>());
+        }
+
+        public DbSet<Message> Messages { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.AddFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
