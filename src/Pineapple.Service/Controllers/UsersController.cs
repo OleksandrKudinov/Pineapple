1652d02f18c38833d6285b8a9db8cd768094ee54
Oleksandr Kudinov <oleksandr.kudinov.1993@gmail.com>
2017-06-15 02:52:36 +0300

    create usersController

---
+++ src/Pineapple.Service/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Pineapple.Database;
+
+namespace Pineapple.Service.Controllers
+{
+    public sealed class UsersController : BaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> RegisterUser([FromBody]User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                using (var context = new PineappleContext(_connectionString))
+                {
+                    context.Users.Add(user);
+                    await context.SaveChangesAsync();
+                    return Ok(user);
+                }
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception);
+            }
+        }
+    }
+}
