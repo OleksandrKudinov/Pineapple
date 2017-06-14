3048067239aa964168ca6f292d9da1f4ba4de58d
Oleksandr Kudinov <oleksandr.kudinov.1993@gmail.com>
2017-06-14 23:20:59 +0300

    add Message database config

---
+++ src/Pineapple.Database/Fluent/MessageEntityConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using Pineapple.Database.Models;
+
+namespace Pineapple.Database.Fluent
+{
+    internal sealed class MessageEntityConfiguration  : EntityTypeConfiguration<Message>
+    {
+        public MessageEntityConfiguration()
+        {
+            var message = this;
+            message.HasKey(x => x.MessageId);
+            message.Property(x => x.Text).IsRequired().HasColumnType("nvarchar");
+        }
+    }
+}
