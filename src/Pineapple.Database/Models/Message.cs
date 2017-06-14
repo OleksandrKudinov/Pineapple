f70145a88f384f2dd5112fc49e40ba4d714e0311
Oleksandr Kudinov <oleksandr.kudinov.1993@gmail.com>
2017-06-15 02:50:27 +0300

    update message model

--- src/Pineapple.Database/Models/Message.cs
+++ src/Pineapple.Database/Models/Message.cs
@@ -5,6 +5,9 @@ namespace Pineapple.Database.Models
     public sealed class Message
     {
         public Int32 MessageId { get; set; }
+        public Int32 UserFromId { get; set; }
+        public Int32 UserToId { get; set; }
         public String Text { get; set; }
+        public DateTime SendDate { get; set; }
     }
 }
