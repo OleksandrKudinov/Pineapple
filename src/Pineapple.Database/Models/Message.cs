b4dac09272541de5772f62056593b5a6eec14b2e
Oleksandr Kudinov <oleksandr.kudinov.1993@gmail.com>
2017-06-14 23:20:18 +0300

    create Message model

---
+++ src/Pineapple.Database/Models/Message.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Pineapple.Database.Models
+{
+    public sealed class Message
+    {
+        public Int32 MessageId { get; set; }
+        public String Text { get; set; }
+    }
+}
