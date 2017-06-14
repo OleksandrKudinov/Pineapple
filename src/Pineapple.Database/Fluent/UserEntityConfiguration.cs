207e568ead3094a9c8c3ac46985148aa5d82e491
Oleksandr Kudinov <oleksandr.kudinov.1993@gmail.com>
2017-06-15 02:49:59 +0300

    add User db model configurator

---
+++ src/Pineapple.Database/Fluent/UserEntityConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+
+namespace Pineapple.Database.Fluent
+{
+    internal sealed class UserEntityConfiguration : EntityTypeConfiguration<User>
+    {
+        public UserEntityConfiguration()
+        {
+            var message = this;
+            message.HasKey(x => x.UserId);
+            message.Property(x => x.Name).IsRequired();
+        }
+    }
+}
