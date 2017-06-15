1313fd80e64aec35a78d59d40349859d0a2600b1
Oleksandr Kudinov <oleksandr.kudinov.1993@gmail.com>
2017-06-16 01:05:01 +0300

    add accountRegisterBindingModel;

---
+++ src/Pineapple.Service/Models/Binding/AccountRegisterBindingModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Pineapple.Service.Models.Binding
+{
+    public sealed class AccountRegisterBindingModel
+    {
+        [Required]
+        public String Username { get; set; }
+
+        [Required]
+        public String Password { get; set; }
+    }
+}
