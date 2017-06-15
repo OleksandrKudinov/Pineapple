7a9691db59b10a15d77ee7fc4e7fda9a1e28fa20
Oleksandr Kudinov <oleksandr.kudinov.1993@gmail.com>
2017-06-15 23:01:15 +0300

    create new entity : Account

---
+++ src/Pineapple.Database/Models/Account.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pineapple.Database.Models
+{
+    public sealed class Account
+    {
+        public Int32 AccountId { get; set; }
+        public User User { get; set; }
+        public String Login { get; set; }
+        public String PasswordHash { get; set; }
+    }
+}
