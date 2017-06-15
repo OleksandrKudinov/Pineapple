e627139e6070135c2fadf0a5380affcaee5d6cec
Oleksandr Kudinov <oleksandr.kudinov.1993@gmail.com>
2017-06-15 18:51:25 +0300

    add jwt middleware

---
+++ src/Pineapple.Service/Infrastructure/Authorization/TokenProviderMiddleware.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+
+namespace Pineapple.Service.Infrastructure.Authorization
+{
+    public sealed class TokenProviderMiddleware
+    {
+        public TokenProviderMiddleware(RequestDelegate next, IOptions<TokenProviderOptions> options)
+        {
+            _next = next;
+            _options = options.Value;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            if (!context.Request.Path.Equals(_options.Path, StringComparison.Ordinal))
+            {
+                Task requestDelegate = _next(context);
+                return requestDelegate;
+            }
+
+            // Request must be POST with Content-Type: application/x-www-form-urlencoded
+            if (!context.Request.Method.Equals("POST")
+               || !context.Request.HasFormContentType)
+            {
+                context.Response.StatusCode = 400;
+                return context.Response.WriteAsync("Bad request.");
+            }
+
+            return GenerateToken(context);
+        }
+
+        private async Task GenerateToken(HttpContext context)
+        {
+            String username = context.Request.Form["login"];
+            String password = context.Request.Form["password"];
+
+            var identity = await GetIdentity(username, password);
+            if (identity == null)
+            {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync("Invalid username or password.");
+                return;
+            }
+
+            var now = DateTime.UtcNow;
+
+            // Specifically add the jti (random nonce), iat (issued timestamp), and sub (subject/user) claims.
+            DateTimeOffset offset = new DateTimeOffset(now);
+
+            var claims = new Claim[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, username),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Iat, offset.ToUnixTimeSeconds().ToString(), ClaimValueTypes.Integer64)
+            };
+
+            var jwt = new JwtSecurityToken(
+                issuer: _options.Issuer,
+                audience: _options.Audience,
+                claims: claims,
+                notBefore: now,
+                expires: now.Add(_options.Expiration),
+                signingCredentials: _options.SigningCredentials);
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            var response = new
+            {
+                access_token = encodedJwt,
+                expires_in = (int)_options.Expiration.TotalSeconds
+            };
+
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsync(JsonConvert.SerializeObject(response, new JsonSerializerSettings { Formatting = Formatting.Indented }));
+        }
+
+        private Task<ClaimsIdentity> GetIdentity(string username, string password)
+        {
+            // TODO replace with usermanager
+            if (username == "user" && password == "password")
+            {
+                return Task.FromResult(new ClaimsIdentity(new System.Security.Principal.GenericIdentity(username, "Token"), new Claim[] { }));
+            }
+
+            return Task.FromResult<ClaimsIdentity>(null);
+        }
+
+        private readonly RequestDelegate _next;
+        private readonly TokenProviderOptions _options;
+    }
+}
---
+++ src/Pineapple.Service/Infrastructure/Authorization/TokenProviderOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Pineapple.Service.Infrastructure.Authorization
+{
+    public sealed class TokenProviderOptions
+    {
+        public String Path { get; set; }
+
+        public String Issuer { get; set; }
+
+        public String Audience { get; set; }
+
+        public TimeSpan Expiration { get; set; }
+
+        public SigningCredentials SigningCredentials { get; set; }
+    }
+}
