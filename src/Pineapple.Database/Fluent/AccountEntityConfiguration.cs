51c61c61595b688ac1a837c0a235b8f918c528f4
Oleksandr Kudinov <oleksandr.kudinov.1993@gmail.com>
2017-06-15 23:02:13 +0300

    added EF configuration for Account

---
+++ src/Pineapple.Database/Fluent/AccountEntityConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using Pineapple.Database.Models;
+
+namespace Pineapple.Database.Fluent
+{
+    internal sealed class AccountEntityConfiguration : EntityTypeConfiguration<Account>
+    {
+        public AccountEntityConfiguration()
+        {
+            var account = this;
+            account.HasKey(x => x.AccountId);
+            account.Property(x => x.Login).IsRequired().HasColumnType("varchar");
+            account.Property(x => x.PasswordHash).IsRequired().HasColumnType("varchar");
+        }
+    }
+}
+
