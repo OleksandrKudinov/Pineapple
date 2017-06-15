5fe03a713bef5896b5813c5844e186dba75d2d04
Oleksandr Kudinov <oleksandr.kudinov.1993@gmail.com>
2017-06-15 22:52:38 +0300

    add authorization for all controllers;

--- src/Pineapple.Service/Controllers/BaseController.cs
+++ src/Pineapple.Service/Controllers/BaseController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Pineapple.Service.Controllers
 {
     [Route("api/[controller]")]
+    [Authorize]
     public abstract class BaseController : Controller
     {
         protected String _connectionString = "";
