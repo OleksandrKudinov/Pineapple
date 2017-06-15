b76c44978f6a0b70f01fbb69de1c45f700ea8427
Oleksandr Kudinov <oleksandr.kudinov.1993@gmail.com>
2017-06-16 01:04:44 +0300

    add account controller

---
+++ src/Pineapple.Service/Controllers/AccountsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Pineapple.Database.Models;
+using Pineapple.Service.Models.Binding;
+
+namespace Pineapple.Service.Controllers
+{
+    public sealed class AccountsController : BaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> CreateAccount([FromBody] AccountRegisterBindingModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                using (var context = RequestDbContext)
+                {
+                    var account = new Account()
+                    {
+                        Login = model.Username,
+                        PasswordHash = model.Password,
+                        User = new User()
+                        {
+                            UserName = model.Username
+                        }
+                    };
+
+                    context.Accounts.Add(account);
+                    await context.SaveChangesAsync();
+
+                    return Ok(new
+                        {
+                            account.User.UserName,
+                            account.PasswordHash
+                        });
+                }
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception);
+            }
+        }
+    }
+}
