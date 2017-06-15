2f021c7ace1b22f86a44b72b039311c277e2b7eb
Oleksandr Kudinov <oleksandr.kudinov.1993@gmail.com>
2017-06-15 22:47:53 +0300

    create LoginViewModel for authorization;

---
+++ src/Pineapple.Service/Models/Binding/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Pineapple.Service.Models.Binding
+{
+    public sealed class LoginViewModel
+    {
+        [Required]
+        public String Username { get; set; }
+
+        [Required]
+        public String Password { get; set; }
+    }
+}
