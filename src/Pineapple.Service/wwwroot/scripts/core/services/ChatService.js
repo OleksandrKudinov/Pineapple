25dbd2c268494ffbfe586f1cdabea37d1b69152c
Oleksandr Kudinov <oleksandr.kudinov.1993@gmail.com>
2017-06-16 18:00:31 +0300

    add chat service

---
+++ src/Pineapple.Service/wwwroot/scripts/core/services/ChatService.js
@@ -0,0 +1,28 @@
+﻿angular
+    .module("ChatServiceModule")
+    .factory("ChatService",
+        [
+            "RequestCallbackInjector",
+            "AuthorizationService",
+            "EntryPointProvider",
+            function (requestCallbackInjector, authorizeService, entryPointProvider) {
+                var service = {};
+                
+                service.getChatsAsync = function (callback) {
+
+                    var url = entryPointProvider.GetEntryPoint() +
+                        "api/chats";
+
+                    var request = {
+                        method: "get",
+                        url: url
+                    };
+
+                    requestCallbackInjector.InjectCallbackToRequest(request, callback);
+                    authorizeService.AuthorizeRequest(request);
+                    $.ajax(request);
+                };
+                
+                return service;
+            }
+        ]);
