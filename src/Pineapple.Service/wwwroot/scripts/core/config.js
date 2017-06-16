110e9906acc0473a785101bd523751c1c67de74d
Oleksandr Kudinov <oleksandr.kudinov.1993@gmail.com>
2017-06-16 17:58:31 +0300

    add modules for angular app

---
+++ src/Pineapple.Service/wwwroot/scripts/core/config.js
@@ -0,0 +1,7 @@
+﻿angular.module("EntryPointProviderModule", []);
+angular.module("RequestCallbackInjectorModule", []);
+angular.module("AuthorizationServiceModule", ["EntryPointProviderModule", "RequestCallbackInjectorModule"]);
+
+angular.module("ChatServiceModule", ["AuthorizationServiceModule", "RequestCallbackInjectorModule", "EntryPointProviderModule"]);
+
+angular.module("PineappleModule", ["ChatServiceModule", "AuthorizationServiceModule"]);
