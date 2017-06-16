17be37d0ac389c439a65ec38a13256505ad3eb4a
Oleksandr Kudinov <oleksandr.kudinov.1993@gmail.com>
2017-06-17 00:24:23 +0300

    add method for getting messages from chat

--- src/Pineapple.Service/wwwroot/scripts/core/services/ChatService.js
+++ src/Pineapple.Service/wwwroot/scripts/core/services/ChatService.js
@@ -15,6 +15,15 @@
                 service.SendAuth(request, callback);
             };
 
+            service.getMessagesFromChat = function (chatId, callback) {
+                var request = {
+                    url: "api/chats/" + chatId + "/messages",
+                    method:"get"
+                }
+
+                service.SendAuth(request, callback);
+            };
+
             return service;
         }
     ]);
