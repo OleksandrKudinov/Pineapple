35d429bacd409984cff5a9e364530673fc0547cf
Oleksandr Kudinov <oleksandr.kudinov.1993@gmail.com>
2017-06-17 00:19:36 +0300

    move auth view from app.js into authpanel directive;

--- src/Pineapple.Service/wwwroot/scripts/core/app.js
+++ src/Pineapple.Service/wwwroot/scripts/core/app.js
@@ -3,30 +3,3 @@
         login: "login",
         logout: "logout"
     };
-
-angular.module("PineappleModule")
-    .controller("AuthController",
-    [
-        "AuthorizationService",
-        "$scope",
-        function (authService, $scope) {
-            var context = this;
-            context.credentials = {};
-
-            context._loginCallback = function (isOk, data) {
-                if (isOk) {
-                    authService.Setup(data);
-                }
-            };
-
-            context.login = function () {
-                console.log(context.credentials);
-                authService.Login(context.credentials, context._loginCallback);
-                context.credentials = {};
-            };
-
-            context.logout = function () {
-                authService.Logout();
-            };
-        }]
-    );
---
+++ src/Pineapple.Service/wwwroot/scripts/core/directives/AuthPanel.js
@@ -0,0 +1,33 @@
+﻿angular
+    .module("PineappleModule")
+    .directive("authPanel",
+    [
+        "AuthorizationService",
+        function (authService) {
+            return {
+                controllerAs: "ctrl",
+                templateUrl: "/templates/authPanel.html",
+                restrict: "A",
+                controller: function ($scope) {
+                    var context = this;
+                    context.credentials = {};
+
+                    context._loginCallback = function (isOk, data) {
+                        if (isOk) {
+                            authService.Setup(data);
+                        }
+                    };
+
+                    context.login = function () {
+                        console.log(context.credentials);
+                        authService.Login(context.credentials, context._loginCallback);
+                        context.credentials = {};
+                    };
+
+                    context.logout = function () {
+                        authService.Logout();
+                    };
+                }
+            };
+        }
+    ]);
