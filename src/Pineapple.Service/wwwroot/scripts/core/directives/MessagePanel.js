637b0dfc2834e37b20692bf50801df77415f9d00
Oleksandr Kudinov <oleksandr.kudinov.1993@gmail.com>
2017-06-17 00:58:14 +0300

    add messagePanel directive with template;

---
+++ src/Pineapple.Service/wwwroot/scripts/core/directives/MessagePanel.js
@@ -0,0 +1,40 @@
+﻿angular
+    .module("PineappleModule")
+    .directive("messagePanel",
+    [
+        "ChatService",
+        function (chatService) {
+            return {
+                controllerAs: "msgCtrl",
+                templateUrl: "/templates/messagePanel.html",
+                restrict: "A",
+                controller: function ($scope, $rootScope) {
+                    var context = this;
+
+                    context.messages = [];
+
+                    context._getMessagesFromChatCallback = function (isOk, messages) {
+                        if (isOk) {
+                            context.messages = messages;
+                            $scope.$apply();
+                        } else {
+                            console.log("messages from chat: =(");
+                            console.log(messages);
+                        }
+                    };
+
+                    context.getMessagesFromChat = function (chatId) {
+                        console.log("chatId: ");
+                        console.log(chatId);
+                        chatService.getGetMessagesFromChatAsync(chatId, context._getMessagesFromChatCallback);
+                    };
+
+                    $scope.$on(
+                        GLOBALEVENTS.chatSelected,
+                        function (event, args) {
+                            context.getMessagesFromChat(args);
+                        });
+                }
+            };
+        }
+    ]);
