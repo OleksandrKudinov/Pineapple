fa92440738e95e65c698993da043284d4b864f86
Oleksandr Kudinov <oleksandr.kudinov.1993@gmail.com>
2017-06-16 18:00:19 +0300

    add request authorization service;

---
+++ src/Pineapple.Service/wwwroot/scripts/core/utils/AuthorizationService.js
@@ -0,0 +1,60 @@
+﻿angular
+    .module("AuthorizationServiceModule")
+    .factory("AuthorizationService",
+        [
+            "RequestCallbackInjector",
+            "EntryPointProvider",
+            function (requestCallbackInjector, entryPointProvider) {
+                var service = {};
+
+                service.AuthorizeRequest = function (request) {
+
+                    if (!request.headers) {
+                        request.headers = {};
+                    }
+
+                    var token = localStorage.getItem("access_token");
+
+                    if (token === null) {
+                        console.log("NON AUTHORIZED!");
+                        return;
+                    }
+
+                    request.headers["Authorization"] = "Bearer " + token;
+
+                    //TODO refactor this
+                    if (request.method == "post" || request.method == "POST") {
+                        request.headers["Content-type"] = "application/json";
+                    }
+                };
+
+                service.Setup = function (data) {
+                    localStorage.setItem("access_token", data.access_token);
+                    localStorage.setItem("expires", data.expires);
+                };
+
+                service.Login = function(credentials, callback) {
+                    var url = entryPointProvider.GetEntryPoint() +
+                        "auth/token";
+
+                    var request = {
+                        data: JSON.stringify(credentials),
+                        method: "post",
+                        url: url
+                    };
+                    //TODO refactor this
+                    if (request.method == "post" || request.method == "POST") {
+                        if (!request.headers) {
+                            request.headers = {};
+                        }
+                        request.headers["Content-type"] = "application/json";
+                    }
+
+                    requestCallbackInjector.InjectCallbackToRequest(request, callback);
+
+                    $.ajax(request);
+                };
+
+                return service;
+            }]
+    );
