9f41b9ff84fe12ce3db4dffe863499dc946be04f
Oleksandr Kudinov <oleksandr.kudinov.1993@gmail.com>
2017-06-16 15:30:56 +0300

    add masterkey validation (temporary feature);

--- src/Pineapple.Service/Controllers/BaseController.cs
+++ src/Pineapple.Service/Controllers/BaseController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Pineapple.Database;
 
 namespace Pineapple.Service.Controllers
@@ -8,6 +10,14 @@ namespace Pineapple.Service.Controllers
     [Authorize]
     public abstract class BaseController : Controller
     {
+        protected Boolean IsMasterKeyValid(String masterkey)
+        {
+            var mk = config["MasterKey"];
+            return mk == masterkey;
+        }
+
+        private IConfigurationRoot config => (IConfigurationRoot)this.HttpContext.RequestServices.GetService(typeof(IConfigurationRoot));
+
         protected PineappleContext RequestDbContext => (PineappleContext)this.HttpContext.RequestServices.GetService(typeof(PineappleContext));
     }
 }
